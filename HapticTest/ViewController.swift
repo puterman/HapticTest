de57f30dfa0cd14df3e4b162235eede1f1cbd308
Linus Åkerlund <linus.akerlund@bontouch.com>
2019-06-17 09:23:11 +0200

    Boiler-plate.

---
+++ HapticTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HapticTest
+//
+//  Created by Linus Akerlund on 2019-06-17.
+//  Copyright © 2019 Puterman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
